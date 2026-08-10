2f44889f73e2152a8ce07cf04479410be90be91f
obitech <clintcodeprogrammer@gmail.com>
2026-08-10 14:51:46 +0100

    test

--- app/admin/manage/instituitions/AdminManageInstituitions.tsx
+++ app/admin/manage/instituitions/AdminManageInstituitions.tsx
@@ -20,9 +20,8 @@ function AdminManageInstituitions() {
             style={{
                 border: navManageInstituitions.add ? "none" : "",
                 gridArea: navManageInstituitions.add ? "1/ 1/ 1/ 1" : "",
-                fontSize: navManageInstituitions.add ? "1.2rem" : "",
             }}>
-                {!navManageInstituitions.add ? "add" : "add\ninstituitions"}
+                {!navManageInstituitions.add ? "add" : "add instituitions"}
                 {!navManageInstituitions.add ? <Plus /> : ""}
             </button>
 
@@ -31,9 +30,8 @@ function AdminManageInstituitions() {
             style={{
                 border: navManageInstituitions.view ? "none" : "",
                 gridArea: navManageInstituitions.view ? "1/ 1/ 1/ 1" : "",
-                fontSize: navManageInstituitions.view ? "1.5rem" : "",
             }}>
-                {!navManageInstituitions.view ? "modify" : "modify\ninstituitions"}
+                {!navManageInstituitions.view ? "modify" : "modify instituitions"}
                 {!navManageInstituitions.view ? <Settings2 /> : ""}
             </button>
         </div>
