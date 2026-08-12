84294b2d680dd714f2c1119e21d0644e8393d57f
obitech <clintcodeprogrammer@gmail.com>
2026-08-12 20:38:51 +0100

    styled

--- app/components/headers/AdminAuthHeader.tsx
+++ app/components/headers/AdminAuthHeader.tsx
@@ -1,27 +1,35 @@
 "use client";
 
 import { AppTheme } from "@/ui/Theme";
-import { useTheme } from "next-themes";
-import Image from "next/image";
-import DarkLogo from "@/public/DarkLogo.png";
-import LightLogo from "@/public/LightLogo.png";
+import { LogoWithName } from "@/ui/Logo";
+import { ChevronDown, ChevronUp, LucideHome } from "lucide-react";
+import { useState } from "react";
 
 function AdminAuthHeader() {
 
-    const { theme } = useTheme();
+    const [showNav, setShowNav] = useState(false);
+
     return (
         <header>
-        <div className="logo">
-            {theme !== "dark" ? (
-                <Image src={DarkLogo} alt="" width={70} height={70} style={{objectFit: "contain"}}/>
-            ) : (
-                <Image src={LightLogo} alt="" width={70} height={70} style={{objectFit: "contain"}}/>
-            )}
-                 nursingpastco
-                </div>
-            <div className="navs">
-                <AppTheme />
+            <div>
+            <div>
+                <LogoWithName />
             </div>
+            <div>
+                <span onClick={() => setShowNav(!showNav)}>
+                     {showNav ? <ChevronDown /> : <ChevronUp />}
+                </span>
+            </div>
+        </div>
+        
+        {showNav && (
+            <nav>
+                <div>
+                    <LucideHome />
+                    <AppTheme />
+                </div>
+            </nav>
+        )}
         </header>
     )
 }
